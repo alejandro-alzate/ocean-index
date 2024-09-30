a844004e8b4fc9b6cebc56ad66d090d7d8af51cc
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-09-30 17:28:10 -0500

    added menori

--- .oceanmanifests/Menori/latest/manifest.lua
+++ .oceanmanifests/Menori/latest/manifest.lua
@@ -1,16 +1,22 @@
 local oceanspec = {
-	package = "",
-	author = "",
-	version = "",
+	package = "Menori",
+	author = "rozenmad",
+	version = "latest",
 	source = {
-		url = ""
+		url = "https://github.com/rozenmad/Menori"
 	},
 
 	description = {
-		summary = "",
-		detailed = [[]],
+		summary = "Library for 3D rendering with LÖVE. ",
+		detailed = [[
+			Menori
+			LÖVE library for 3D rendering based on scene graph. Support glTF 2.0 (implemented: meshes, materials, textures, skins, skeletons, animations). Assets may be provided either in JSON (.gltf) or binary (.glb) format.
+			Works on LÖVE 11.4 and higher.
+		]],
 		homepage = "",
 	},
+
+	love = ">= 11.4"
 
 }
 

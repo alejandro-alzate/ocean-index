211b0c764d894a3c161832c01b97843e5ffbb3c9
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-09-30 12:34:39 -0500

    added manifests folder

---
+++ .oceanmanifests/love/11.5/manifest.lua
@@ -0,0 +1,41 @@
+-- This is a demostration of a beta package manager for lua and love.
+
+local oceanspec = {
+	package = "love",
+	author = "alejandro-alzate",
+	version = "11.5",
+	source = {
+		url = "https://github.com/love2d/love"
+	},
+
+	--Platform specific optional
+	architectures = {
+		source = "https://github.com/love2d/love/archive/refs/tags/11.5.zip",
+		android = "https://github.com/love2d/love/releases/download/11.5/love-11.5-android.apk",
+		linuxsrc = "https://github.com/love2d/love/releases/download/11.5/love-11.5-linux-src.tar.gz",
+		macos = "https://github.com/love2d/love/releases/download/11.5/love-11.5-macos.zip",
+		win64 = "https://github.com/love2d/love/releases/download/11.5/love-11.5-win64.zip",
+		win32 = "https://github.com/love2d/love/releases/download/11.5/love-11.5-win32.zip",
+		appimg = "https://github.com/love2d/love/releases/download/11.5/love-11.5-x86_64.AppImage",
+	},
+	description = {
+		summary = "LÖVE is an awesome 2D game framework for Lua.",
+		detailed =
+		"LÖVE is an *awesome* framework you can use to make 2D games in Lua. It's free, open-source, and works on Windows, macOS, Linux, Android, and iOS.",
+		homepage = "love2d.org",
+		license = "https://github.com/love2d/love/blob/main/license.txt",
+	},
+
+	--It's love.
+	--lua = ">= 5.1, < 5.4",
+
+	--It's love.
+	--love = ">=11.0 < 12",
+
+	dependency = {
+		-- None, self contained. :)
+	}
+
+}
+
+return oceanspec
---
+++ .oceanmanifests/ocean/1.0/manifest.lua
@@ -0,0 +1,11 @@
+local oceanspec = {
+	package = "ocean",
+	author = "alejandro-alzate",
+	version = "1.0",
+	source = {
+		url = "https://github.com/alejandro-alzate/ocean/"
+	},
+
+}
+
+return oceanspec

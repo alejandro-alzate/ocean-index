3482a17110469c51762195f4b27d0855b4fd91e1
Alejandro Alzate Sánchez <alejandroalzatesanchez@gmail.com>
2024-09-30 14:56:11 -0500

    updated indexes some specs new

---
+++ .oceanmanifests/language/latest/manifest.lua
@@ -0,0 +1,21 @@
+local oceanspec = {
+	package = "ocean",
+	author = "alejandro-alzate",
+	version = "1.0",
+	source = {
+		url = "https://github.com/alejandro-alzate/language-lua"
+	},
+
+	description = {
+		summary =
+		" A pure lua language manager geared to use on user interfaces whether is a game or a productivity tool with capability of hotswaping",
+		detailed = [[A pure lua library for management of strings geared towards localization purposes.]]
+	},
+
+	dependencies = {
+
+	}
+
+}
+
+return oceanspec
